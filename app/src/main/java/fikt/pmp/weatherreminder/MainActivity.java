5549f1253077a7672b2058e8b7f52bacdf322ed5
Ivana <i.krivcevska@gmail.com>
2019-06-03 22:24:43 +0200

    Show weather picture using Picasso

--- app/src/main/java/fikt/pmp/weatherreminder/MainActivity.java
+++ app/src/main/java/fikt/pmp/weatherreminder/MainActivity.java
@@ -4,7 +4,10 @@ import android.graphics.Path;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import fikt.pmp.weatherreminder.DataModel.List;
 import fikt.pmp.weatherreminder.DataModel.OpenWeatherMapData;
@@ -18,13 +21,23 @@ public class MainActivity extends AppCompatActivity {
     private static String BASE_URL = "http://api.openweathermap.org/";
     private static String APP_ID = "929c4499e37f7764ce3dda29873feb84";
     private static String ID = "792578";
+    private static String ICON_BASE_URL = "http://openweathermap.org/img/w/";
+
     private TextView weatherData;
+    private TextView cityName;
+    private TextView country;
+    private ImageView weatherIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         weatherData = findViewById(R.id.weatherData);
+        cityName = findViewById(R.id.cityName);
+        country = findViewById(R.id.countryName);
+        weatherIcon = findViewById(R.id.weatherIcon);
+
         getCurrentData();
     }
 
@@ -41,7 +54,13 @@ public class MainActivity extends AppCompatActivity {
                 if (response.code() == 200) {
                     OpenWeatherMapData weatherResponse = response.body();
                     assert weatherResponse != null;
-                    String stringBuilder = "Location: " ;
+
+                    cityName.setText(weatherResponse.getCity().getName());
+                    country.setText(", " + weatherResponse.getCity().getCountry());
+                    String iconCode = weatherResponse.getList().get(0).getWeather().get(0).getIcon();
+                    String url = ICON_BASE_URL+iconCode+".png";
+                    Picasso.get().load(url).into(weatherIcon);
+                    String stringBuilder = "" ;
                     for (int i = 0; i < weatherResponse.getCnt(); i++) {
                         List item = weatherResponse.getList().get(i);
                         stringBuilder += "Time: " +
