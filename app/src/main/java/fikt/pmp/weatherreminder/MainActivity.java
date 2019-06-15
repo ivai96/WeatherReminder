ca298756cd0b2d20e77779ccaa040f5613b48a6d
Ivana <i.krivcevska@gmail.com>
2019-06-15 19:36:38 +0200

    Add a menu for sign in

--- app/src/main/java/fikt/pmp/weatherreminder/MainActivity.java
+++ app/src/main/java/fikt/pmp/weatherreminder/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
@@ -31,6 +33,7 @@ import fikt.pmp.weatherreminder.ConsumingAPIs.FiveDayThreeHour;
 import fikt.pmp.weatherreminder.DataModel.List;
 import fikt.pmp.weatherreminder.DataModel.OpenWeatherMapCurrent;
 import fikt.pmp.weatherreminder.DataModel.OpenWeatherMapFiveDays;
+import fikt.pmp.weatherreminder.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -245,4 +248,20 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItem = item.getItemId();
+        if(menuItem == R.id.signIn){
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
